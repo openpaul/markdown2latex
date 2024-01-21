41defb2102b73f7262a68a05eceffb18790db9e6
Paul Saary <github@paulsaary.de>
2024-01-21 11:40:05 +0100

    parameterize test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -7,11 +7,12 @@ from mdx_latex import (
     unescape_latex_entities,
 )
 import markdown
+import pytest
 
 
 def test_inline_html_latex():
     input_text = "&ldquo;Sample text&rdquo; &lsquo;Another text&rsquo; &laquo;Yet another text&raquo; ..."
-    expected_output = "\\enquote{Sample text} \\enquote{Another text} \\enquote{Yet another text} \dots"
+    expected_output = "\\enquote{Sample text} \\enquote{Another text} \\enquote{Yet another text} \\dots"
 
     result = inline_html_latex(input_text)
 
@@ -50,24 +51,6 @@ def test_makeExtension():
     assert isinstance(ext, LaTeXExtension)
 
 
-def test_basic():
-    text = "Simple test"
-    output = markdown.markdown(text, extensions=[LaTeXExtension()])
-
-    expected_output = "Simple test"
-
-    assert output == expected_output
-
-
-def test_header():
-    text = "## Simple test"
-    output = markdown.markdown(text, extensions=[LaTeXExtension()])
-
-    expected_output = "\\section{Simple test}"
-
-    assert output == expected_output
-
-
 def test_items():
     markdown_text = """## A first section ##
 
@@ -90,38 +73,22 @@ A simple list:
     )
 
 
-def test_math_basic():
-    # simple inline math
-    simple_md = "This is $1+2$ inline math."
-    expected = "This is \\(1+2\\) inline math."
-    output = markdown.markdown(simple_md, extensions=[LaTeXExtension()])
-    assert output == expected
 
-    # simple block math
-    simple_md = "This is $$1+2$$ inline math."
-    expected = "This is \\[1+2\\] inline math."
-    output = markdown.markdown(simple_md, extensions=[LaTeXExtension()])
-    assert output == expected
-
-    # a simple dollar sign
-    simple_md = "I only have $13 left."
-    expected = "I only have \\$13 left."
-    output = markdown.markdown(simple_md, extensions=[LaTeXExtension()])
-    assert output == expected
-
-    # All together now
-    markdown_text = "Some mathematics inline, $$X$$, $Y$, a $100 million, a %tage and then a formula:"
-    expected_output = "Some mathematics inline, \\[X\\], \\(Y\\), a \\$100 million, a \\%tage and then a formula:"
-
+@pytest.mark.parametrize(
+    "markdown_text,expected_output",
+    [
+        ("Hello World", "Hello World"),
+        ("## Hello World", "\\section{Hello World}"),
+        ("### Hello World", "\\subsection{Hello World}"),
+        ("Hello $World$", "Hello \\(World\\)"),
+        ("Hello $$World$$", "Hello \\[World\\]"),
+        ("Hello $World", "Hello \\$World"),
+        ("$$ \\sum_{i}^{\\infty} x^{n} + y^{n} = \\alpha +  \\beta * z^{n} $$",
+         "\\[ \\sum_{i}^{\\infty} x^{n} + y^{n} = \\alpha +  \\beta \\cdot z^{n} \\]"),
+        ("Some mathematics inline, $$X$$, $Y$, a $100 million, a %tage and then a formula:",
+         "Some mathematics inline, \\[X\\], \\(Y\\), a \\$100 million, a \\%tage and then a formula:")
+    ],
+)
+def test_online(markdown_text, expected_output):
     output = markdown.markdown(markdown_text, extensions=[LaTeXExtension()])
-
-    assert output == expected_output
-
-def test_formular():
-
-    md  = "$$ \\sum_{i}^{\\infty} x^{n} + y^{n} = \\alpha +  \\beta * z^{n} $$"
-
-    expected_output = "\\[ \\sum_{i}^{\\infty} x^{n} + y^{n} = \\alpha +  \\beta \cdot z^{n} \\]"
-
-    output = markdown.markdown(md, extensions=[LaTeXExtension()])
     assert output == expected_output
